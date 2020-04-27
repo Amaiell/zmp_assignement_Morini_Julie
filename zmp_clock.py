1a9c6567918459e1c1e4e969500b19406af20316
Amaiell <63179048+Amaiell@users.noreply.github.com>
2020-04-27 11:38:41 +0900

    Add files via upload

--- zmp_clock.py
+++ zmp_clock.py
@@ -1,8 +1,10 @@
-#we suppose that the user is entering the hour in the following format : HH:MM
+#this function ask the hour to the users, supposing that the user enters the hour in a HH:MM format
 def clock_needle():
     time=input('What time is it?:')
-    hour=int(time[0:2])
+    #if the user use a 24hours based notation, the hour is converted to the 12h based notation
+    hour=int(time[0:2])%12
     minute =int(time[3:5])
     hour_angle=hour*360/12
     minute_angle=minute*360/60
-    return(hour_angle-minute_angle)
+    #the returned angle is a postive angle and is the smallest one 
+    return(max(hour_angle,minute_angle)-min(hour_angle,minute_angle))%180
